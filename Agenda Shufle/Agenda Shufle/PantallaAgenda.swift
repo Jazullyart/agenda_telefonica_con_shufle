73ca65130d4feafbbf6b0ccceb232cb0872c2eb5
alumno <alumno@lap-fpizarro.uacj.mx>
2025-02-26 10:50:22 -0700

    [AGREGA] Modelo de contacto y pantalla

--- Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
+++ Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
@@ -7,11 +7,19 @@
 
 import SwiftUI
 
+let contactos = [
+    ContactoAgenda(nombre: "Juan", telefono: "656 123 4567"),
+    ContactoAgenda(nombre: "Pablo", telefono: "656 123 4568"),
+    ContactoAgenda(nombre: "Astrid", telefono: "656 123 4569"),
+    ContactoAgenda(nombre: "Aron", telefono: "656 123 4570")
+]
+
 struct PantallaAgenda: View {
     var body: some View {
         List{
             VStack {
-                ForEach(0...150, id: \.self){ _ in
+                ForEach(contactos){ contacto in
+                    Text("\(contacto.nombre)")
                     ContactoPrevista()
                 }
             }
