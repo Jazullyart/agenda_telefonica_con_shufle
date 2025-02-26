0592d0f881f98139df84dc8af0079fa7e0f1b1cc
alumno <alumno@lap-fpizarro.uacj.mx>
2025-02-26 10:13:52 -0700

    [AGREGA] un componente para la vista del contacto prevista

---
+++ Agenda Shufle/Agenda Shufle/contacto_prevista.swift
@@ -0,0 +1,23 @@
+//
+//  contacto_prevista.swift
+//  Agenda Shufle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContactoPrevista: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "person.fill")
+                .scaledToFit()
+            Text("Jelou darknes")
+            Text("mai ol fren")
+        }
+    }
+}
+
+#Preview {
+    ContactoPrevista()
+}
