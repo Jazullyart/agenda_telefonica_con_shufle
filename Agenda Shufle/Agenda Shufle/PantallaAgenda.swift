e807df8c6ff57d4b65d7b007eec06eab3dbf16b9
alumno <alumno@lap-fpizarro.uacj.mx>
2025-02-28 10:41:19 -0700

    [MODIFICA] la vista del contacto

--- Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
+++ Agenda Shufle/Agenda Shufle/PantallaAgenda.swift
@@ -11,20 +11,33 @@ let contactos = [
     ContactoAgenda(nombre: "Juan", telefono: "656 123 4567"),
     ContactoAgenda(nombre: "Pablo", telefono: "656 123 4568"),
     ContactoAgenda(nombre: "Astrid", telefono: "656 123 4569"),
+    ContactoAgenda(nombre: "Aron", telefono: "656 123 4570"),
+    ContactoAgenda(nombre: "Juan", telefono: "656 123 4567"),
+    ContactoAgenda(nombre: "Pablo", telefono: "656 123 4568"),
+    ContactoAgenda(nombre: "Astrid", telefono: "656 123 4569"),
+    ContactoAgenda(nombre: "Aron", telefono: "656 123 4570"),
+    ContactoAgenda(nombre: "Juan", telefono: "656 123 4567"),
+    ContactoAgenda(nombre: "Pablo", telefono: "656 123 4568"),
+    ContactoAgenda(nombre: "Astrid", telefono: "656 123 4569"),
     ContactoAgenda(nombre: "Aron", telefono: "656 123 4570")
 ]
 
 struct PantallaAgenda: View {
+    var ancho_de_pantalla = UIScreen.main.bounds.width
+    var largo_de_pantalla = UIScreen.main.bounds.height
+    
     var body: some View {
-        List{
-            VStack {
+        ScrollView{
+            VStack(spacing: 10) {
                 ForEach(contactos){ contacto in
-                    Text("\(contacto.nombre)")
-                    ContactoPrevista()
+                    //Text("\(contacto.nombre)")
+                    ContactoPrevista(contacto_a_mostrar: contacto, al_pulsar: {print("Te envia saludos \(contacto.nombre) desde la página agenda")})
                 }
             }
             .background(Color.teal)
+            .frame(width: ancho_de_pantalla, alignment: Alignment.center)
         }
+        .background(Color.mint)
     }
 }
 
--- Agenda Shufle/Agenda Shufle/contacto_prevista.swift
+++ Agenda Shufle/Agenda Shufle/contacto_prevista.swift
@@ -8,16 +8,43 @@
 import SwiftUI
 
 struct ContactoPrevista: View {
+    var contacto_a_mostrar: ContactoAgenda
+    var al_pulsar: () -> Void = { print("No se ha implementado")}
+    
+    let equinas_redondeadas = CGSize(width: 55, height: 55)
+    
     var body: some View {
-        HStack {
-            Image(systemName: "person.fill")
-                .scaledToFit()
-            Text("Jelou darknes")
-            Text("mai ol fren")
+        HStack(alignment: VerticalAlignment.center) {
+            Spacer()
+            
+            VStack{
+                Image("Image 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 75, alignment: .center)
+                    .clipShape(RoundedRectangle(cornerSize: equinas_redondeadas))
+            }
+            .background(Color.blue)
+            
+            Spacer()
+            
+            VStack(alignment: HorizontalAlignment.leading){
+                Text(contacto_a_mostrar.nombre)
+                Text(contacto_a_mostrar.telefono)
+            }
+            .background(Color.gray)
+            .frame(width: 150)
+            
+            Spacer()
+        }
+        .frame(width: 250)
+        .background(Color.pink)
+        .onTapGesture {
+            al_pulsar()
         }
     }
 }
 
 #Preview {
-    ContactoPrevista()
+    ContactoPrevista(contacto_a_mostrar: ContactoAgenda(nombre: "Nombre", telefono: "Número telefónico"))
 }
